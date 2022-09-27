3b3a13b1a7351aa7835ab49aa29f2cc8167363f4
ofemjohn <ofemjohn@gmail.com>
2022-09-27 21:38:24 +0100

    open a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Contains the text function for reading a text file.
+
+'''
+def read_file(filename=""):
+    ''' Reading a file from the text file UTF8'''
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
+
