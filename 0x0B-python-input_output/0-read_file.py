425cde6ebee3a42b9992a9401e638c0d0f48add7
ofemjohn <ofemjohn@gmail.com>
2022-09-27 21:43:10 +0100

    read a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,8 +3,9 @@
 Contains the text function for reading a text file.
 
 '''
+
+
 def read_file(filename=""):
     ''' Reading a file from the text file UTF8'''
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
-
