1f6d1ab1066bf88f2692fa61918a119ef0c06be8
ofemjohn <ofemjohn@gmail.com>
2022-11-28 19:45:59 +0100

    console print

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* script that prints three lines */
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log ('C is fun');
+console.log ('Python is cool');
+console.log ('JavaScript is amazing');
 
 
 
