aefefe619a911932e7c4bc57b77608d615421944
ofemjohn <ofemjohn@gmail.com>
2022-12-04 20:58:29 +0100

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
+"""This module contains a script that displays the states table"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
-
-''' script that takes in argument and display all values
-in the state table
-'''
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3],
-        charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINRY'{}' ORDERED BY id ASC"
-            .format(sys.argv[4]))
-    fetch = cur.fetchall()
-    for i in fetch:
-        print(i)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE Name LIKE BINARY '{}' ORDER BY id ASC"
+        .format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
-    db.close()
+    conn.close()
