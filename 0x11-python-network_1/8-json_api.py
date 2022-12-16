87525dede11a898616e26ffd9337a21270d524d9
ofemjohn <ofemjohn@gmail.com>
2022-12-16 23:38:41 +0100

    fix

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
+"""takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter
+as a parameter
 """
-Python script that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter
-"""
-import requests
-from sys import argv
-
-if __name__ == '__main__':
-    q = argv[1] if len(argv) == 2 else ""
+if __name__ == "__main__":
+    import sys
+    import requests
+    if len(sys.argv) > 1:
+        value = sys.argv[1]
+    else:
+        value = ""
+    params = {'q': value}
     url = 'http://0.0.0.0:5000/search_user'
-    r = requests.post(url, data={'q': q})
-    try:
-        r_dict = r.json()
-        id, name = r_dict.get('id'), r_dict.get('name')
-        if len(r_dict) == 0 or not id or not name:
+    r = requests.post(url, data=params)
+    if r.headers.get('content-type') == 'application/json':
+        if r.json() == {}:
             print("No result")
         else:
-            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
-    except:
+            id_ = r.json().get('id')
+            name = r.json().get('name')
+            print("[{}] {}".format(id_, name))
+    else:
         print("Not a valid JSON")
